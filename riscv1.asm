3e2f4ffa983f0b0f499d7c7d3422b2fb1fc3ec0d
KelvinVillago <39169401+KelvinVillago@users.noreply.github.com>
2022-03-03 16:59:12 -0800

    Add files via upload

---
+++ riscv1.asm
@@ -0,0 +1,153 @@
+PWM: 
+.data 
+sseg:    .byte  0x03,0x9F,0x25,0x0D,0x99,0x49,0x41,0x1F,0x01,0x09 # LUT for 7-segs
+
+.text
+init: li x20, 0x1100C00C   #SSEG register
+      li x21, 0x09         #last value of LUT
+      la x23, sseg         #LUT address
+      lb x19, 0(x23)       #load first value of LUT
+      sb x19, 0(x20)       #turn on first number
+      
+
+fade:  
+       li    x10, 125000    #10ms delay
+       li    x11, 125000    #10ms delay
+       li    x12, 3         #number of times we delay
+       mv    x30, x10       # copying number of times delayed for on
+       mv    x31, x11       # copying number of times delayed for off
+       call  delay
+       
+       li    x10, 112500   #9ms delay
+       li    x11, 125000   #10ms delay
+       li    x12, 4        #number of times we delay
+       mv    x30, x10      # copying number of times delayed for on
+       mv    x31, x11      # copying number of times delayed for off
+       call  delay
+       
+       li    x10, 100000    #8ms delay
+       li    x11, 125000    #10ms delay
+       li    x12, 5         #number of times we delay
+       mv    x30, x10       # copying number of times delayed for on
+       mv    x31, x11       # copying number of times delayed for off
+       call  delay
+       
+       li    x10, 87500    #7ms delay
+       li    x11, 125000   #10ms delay
+       li    x12, 6        #number of times we delay
+       mv    x30, x10      # copying number of times delayed for on
+       mv    x31, x11      # copying number of times delayed for off
+       call  delay
+       
+       li    x10, 75000    #6ms delay
+       li    x11, 125000   #10ms delay
+       li    x12, 7        #number of times we delay
+       mv    x30, x10      #copying number of times delayed for on
+       mv    x31, x11      #copying number of times delayed for off
+       call  delay
+       
+       li    x10, 62500    #5ms delay
+       li    x11, 125000   #10ms delay
+       li    x12, 8        #number of times we delay
+       mv    x30, x10      #copying number of times delayed for on
+       mv    x31, x11      #copying number of times delayed for off
+       call  delay
+       
+       li    x10, 50000   #4ms delay
+       li    x11, 125000  #10ms delay
+       li    x12, 9       #number of times we delay
+       mv    x30, x10     # copying number of times delayed for on
+       mv    x31, x11     # copying number of times delayed for off
+       call  delay
+       
+       li    x10, 37500   #3ms delay
+       li    x11, 125000  #10ms delay
+       li    x12, 10      #number of times we delay
+       mv    x30, x10     # copying number of times delayed for on
+       mv    x31, x11     # copying number of times delayed for off
+       call  delay
+       
+       li    x10, 25000   #2ms delay
+       li    x11, 125000  #10ms delay
+       li    x12, 11      #number of times we delay
+       mv    x30, x10     # copying number of times delayed for on
+       mv    x31, x11     # copying number of times delayed for off
+       call  delay
+       
+       li    x10, 12500   #1ms delay
+       li    x11, 125000  #10ms delay
+       li    x12, 12      #number of times we delay
+       mv    x30, x10     #copying number of times delayed for on
+       mv    x31, x11     #copying number of times delayed for off
+       call  delay
+       
+       J admin
+
+admin:        
+       li    x10, 1         #0 ms delay
+       li    x11, 500000    #14 ms delay
+       li    x12, 6         #number of times we delay
+       mv    x30, x10       # copying number of times delayed for on
+       mv    x31, x11       # copying number of times delayed for off
+       call  switch_delay
+       
+       j next_num
+
+
+next_num:    addi x23, x23, 1
+	     lb   x19, 0(x23)            
+      	     sb   x19, 0(x20) 
+             beq  x19, x0, reset      #if led is at first one, reset
+                    
+             li    x10, 1         #0msdelay
+             li    x11, 500000    #14 ms delay
+             li    x12, 6         #number of times we delay
+             mv    x30, x10       # copying number of times delayed for on
+             mv    x31, x11       # copying number of times delayed for off
+             call  switch_delay
+             
+             j fade
+
+reset:       addi x23, x23, -9
+             lb x19, 0(x23)
+	     sb x19, 0(x20)
+             j fade
+
+
+switch_delay: addi x30, x10, 0
+
+s_delay: addi x10, x10, -1          #decrement the delay on
+          bne  x10, x0, on_delay    #delay by a0 after turn on
+          
+          add  x10, x10, x30          #reset the delay until loop is done
+          
+          sw x0, 0(x20)             #turn off led
+
+s2_delay: addi x11, x11, -1         #decrement the delay off
+           bne x11, x0, off_delay   #delay by a1 after turn on       
+           add x11, x0, x31		   #reset the delay until loop is done
+           
+           addi x12, x12, -1         #decrement the number of times we delay
+           bne x12, x0, repeat       #Do it again if loop is not done
+           ret                       #Done with the loop
+
+
+delay: addi    x30, x10, 0
+       addi    x31, x11, 0
+
+repeat: sw  x19, 0(x20) #turn on led
+
+on_delay: addi x10, x10, -1          #decrement the delay on
+          bne  x10, x0, on_delay    #delay by a0 after turn on
+          
+          add  x10, x10, x30          #reset the delay until loop is done
+          
+          sw x0, 0(x20)             #turn off led
+
+off_delay: addi x11, x11, -1         #decrement the delay off
+           bne x11, x0, off_delay   #delay by a1 after turn on       
+           add x11, x0, x31		   #reset the delay until loop is done
+           
+           addi x12, x12, -1         #decrement the number of times we delay
+           bne x12, x0, repeat       #Do it again if loop is not done
+           ret                       #Done with the loop
