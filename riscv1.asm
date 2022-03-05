aed53ae366b3124d13fafee811ba5013b93ce4b7
KelvinVillago <39169401+KelvinVillago@users.noreply.github.com>
2022-03-04 17:45:46 -0800

    Add files via upload

--- riscv1.asm
+++ riscv1.asm
@@ -3,11 +3,15 @@ PWM:
 sseg:    .byte  0x03,0x9F,0x25,0x0D,0x99,0x49,0x41,0x1F,0x01,0x09 # LUT for 7-segs
 
 .text
-init: li x20, 0x1100C00C   #SSEG register
+init: li x20, 0x1100C004   #SSEG register
+      li x22, 0x1100C008   #anodes
       li x21, 0x09         #last value of LUT
       la x23, sseg         #LUT address
+      li x14, 0xF
       lb x19, 0(x23)       #load first value of LUT
       sb x19, 0(x20)       #turn on first number
+      li x13, 0x7
+      sb x13, 0(x22)
       
 
 fade:  
@@ -84,12 +88,12 @@ fade:
        J admin
 
 admin:        
-       li    x10, 1         #0 ms delay
+       li    x10, 2         #0 ms delay
        li    x11, 500000    #14 ms delay
        li    x12, 6         #number of times we delay
        mv    x30, x10       # copying number of times delayed for on
        mv    x31, x11       # copying number of times delayed for off
-       call  switch_delay
+       call  delay
        
        j next_num
 
@@ -99,50 +103,31 @@ next_num:    addi x23, x23, 1
       	     sb   x19, 0(x20) 
              beq  x19, x0, reset      #if led is at first one, reset
                     
-             li    x10, 1         #0msdelay
+             li    x10, 2         #0msdelay
              li    x11, 500000    #14 ms delay
              li    x12, 6         #number of times we delay
              mv    x30, x10       # copying number of times delayed for on
              mv    x31, x11       # copying number of times delayed for off
-             call  switch_delay
+             call  delay
              
              j fade
 
-reset:       addi x23, x23, -9
+reset:       addi x23, x23, -10
              lb x19, 0(x23)
 	     sb x19, 0(x20)
              j fade
 
-
-switch_delay: addi x30, x10, 0
-
-s_delay: addi x10, x10, -1          #decrement the delay on
-          bne  x10, x0, on_delay    #delay by a0 after turn on
-          
-          add  x10, x10, x30          #reset the delay until loop is done
-          
-          sw x0, 0(x20)             #turn off led
-
-s2_delay: addi x11, x11, -1         #decrement the delay off
-           bne x11, x0, off_delay   #delay by a1 after turn on       
-           add x11, x0, x31		   #reset the delay until loop is done
-           
-           addi x12, x12, -1         #decrement the number of times we delay
-           bne x12, x0, repeat       #Do it again if loop is not done
-           ret                       #Done with the loop
-
-
 delay: addi    x30, x10, 0
        addi    x31, x11, 0
 
-repeat: sw  x19, 0(x20) #turn on led
+repeat: sw  x13, 0(x22) #turn on led
 
 on_delay: addi x10, x10, -1          #decrement the delay on
           bne  x10, x0, on_delay    #delay by a0 after turn on
           
           add  x10, x10, x30          #reset the delay until loop is done
           
-          sw x0, 0(x20)             #turn off led
+          sw x14, 0(x22)             #turn off led
 
 off_delay: addi x11, x11, -1         #decrement the delay off
            bne x11, x0, off_delay   #delay by a1 after turn on       
